6ebb61aefcbc115ab5bf62f8de8d73004523f6f6
mustafabasheer7 <mustafa.basheer55@gmail.com>
2022-05-14 00:20:10 -0400

    fixed structure of the application

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Home from "./pages/Home";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="">
+      <Home />
+    </h1>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const Home = () => {
+  return (
+    <div className="text-3xl font-bold underline">
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
